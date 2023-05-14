d7566aac7367830cc099e83cedf40c167df7b4f3
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-05-14 12:54:05 -0300

    Create param-fusion.py

---
+++ param-fusion.py
@@ -0,0 +1,24 @@
+import argparse
+
+# Definição dos argumentos da linha de comando
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url-file", help="Caminho para o arquivo com as URLs")
+parser.add_argument("-p", "--param-file", help="Caminho para o arquivo com os parâmetros")
+args = parser.parse_args()
+
+# Leitura dos arquivos de entrada
+with open(args.url_file, "r") as url_file:
+    urls = url_file.read().splitlines()
+
+with open(args.param_file, "r") as param_file:
+    params = param_file.read().splitlines()
+
+# Geração das combinações de URLs e parâmetros
+combinations = []
+for url in urls:
+    for param in params:
+        combinations.append(url + "?" + param + "=")
+
+# Impressão dos resultados
+for combination in combinations:
+    print(combination)
